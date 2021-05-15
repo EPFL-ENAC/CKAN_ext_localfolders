fe1e6bef497da0f6b11a4485b6561c7cf5fd1a38
Corentin Junod <corentin.junod@outlook.com>
2021-05-16 00:41:27 +0200

    Properly set dataset attributes

--- ckanext/localfolders/harvester.py
+++ ckanext/localfolders/harvester.py
@@ -13,6 +13,7 @@ import os
 import logging
 
 log = logging.getLogger(__name__)
+base_url = '/srv/app/data/harvest/'
 
 class LocalFoldersHarvester(HarvesterBase):
 
@@ -42,29 +43,19 @@ class LocalFoldersHarvester(HarvesterBase):
 
   def gather_stage(self, harvest_job):
     '''
-    The gather stage will receive a HarvestJob object and will be
-    responsible for:
-        - gathering all the necessary objects to fetch on a later.
-          stage (e.g. for a CSW server, perform a GetRecords request)
-        - creating the necessary HarvestObjects in the database, specifying
-          the guid and a reference to its job. The HarvestObjects need a
-          reference date with the last modified date for the resource, this
-          may need to be set in a different stage depending on the type of
-          source.
-        - creating and storing any suitable HarvestGatherErrors that may
-          occur.
-        - returning a list with all the ids of the created HarvestObjects.
-        - to abort the harvest, create a HarvestGatherError and raise an
-          exception. Any created HarvestObjects will be deleted.
-
     :param harvest_job: HarvestJob object
     :returns: A list of HarvestObject ids
     '''
-    log.info("In gather stage: %s" % harvest_job.source.url)
+    full_url = base_url+harvest_job.source.url
+
+    log.info("Infos:")
+    log.info(harvest_job.source.__dict__)
+
+    log.info("In gather stage: %s" % full_url)
     objs_ids = []
     counter = 0
 
-    for (root, dirs, files) in os.walk(harvest_job.source.url):
+    for (root, dirs, files) in os.walk(full_url):
 
       resources = []
 
@@ -87,11 +78,11 @@ class LocalFoldersHarvester(HarvesterBase):
         content = {
           "id" : str(root),
           "private" : False,
-          "name" : str(root),
+          "name" : harvest_job.source.title,
           "resources" : resources
         }
 
-        obj = HarvestObject(guid=harvest_job.source.url+str(counter),
+        obj = HarvestObject(guid=full_url+str(counter),
                             job=harvest_job,
                             content=json.dumps(content))
         obj.save()
@@ -104,18 +95,6 @@ class LocalFoldersHarvester(HarvesterBase):
 
   def fetch_stage(self, harvest_object):
     '''
-    The fetch stage will receive a HarvestObject object and will be
-    responsible for:
-        - getting the contents of the remote object (e.g. for a CSW server,
-          perform a GetRecordById request).
-        - saving the content in the provided HarvestObject.
-        - creating and storing any suitable HarvestObjectErrors that may
-          occur.
-        - returning True if everything is ok (ie the object should now be
-          imported), "unchanged" if the object didn't need harvesting after
-          all (ie no error, but don't continue to import stage) or False if
-          there were errors.
-
     :param harvest_object: HarvestObject object
     :returns: True if successful, 'unchanged' if nothing to import after
               all, False if not successful
@@ -140,26 +119,6 @@ class LocalFoldersHarvester(HarvesterBase):
 
   def import_stage(self, harvest_object):
     '''
-    The import stage will receive a HarvestObject object and will be
-    responsible for:
-        - performing any necessary action with the fetched object (e.g.
-          create, update or delete a CKAN package).
-          Note: if this stage creates or updates a package, a reference
-          to the package should be added to the HarvestObject.
-        - setting the HarvestObject.package (if there is one)
-        - setting the HarvestObject.current for this harvest:
-          - True if successfully created/updated
-          - False if successfully deleted
-        - setting HarvestObject.current to False for previous harvest
-          objects of this harvest source if the action was successful.
-        - creating and storing any suitable HarvestObjectErrors that may
-          occur.
-        - creating the HarvestObject - Package relation (if necessary)
-        - returning True if the action was done, "unchanged" if the object
-          didn't need harvesting after all or False if there were errors.
-
-    NB You can run this stage repeatedly using 'paster harvest import'.
-
     :param harvest_object: HarvestObject object
     :returns: True if the action was done, "unchanged" if the object didn't
               need harvesting after all or False if there were errors.
@@ -170,7 +129,7 @@ class LocalFoldersHarvester(HarvesterBase):
     package_dict = json.loads(harvest_object.content)
     package_dict['owner_org'] = self._get_owner(harvest_object)
     result = self._create_or_update_package(package_dict, harvest_object, package_dict_form='package_show')
-    
+
     return result
 
 class NotImplementedError(Exception):
