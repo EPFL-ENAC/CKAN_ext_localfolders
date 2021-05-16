f07b1d2f4fd97e89d1ce2efbef1a32da3de75ed4
Corentin Junod <corentin.junod@outlook.com>
2021-05-16 12:07:28 +0200

    Start implementing tags

--- ckanext/localfolders/harvester.py
+++ ckanext/localfolders/harvester.py
@@ -25,50 +25,43 @@ class LocalFoldersHarvester(HarvesterBase):
       'description': 'Custom harvester for local folders'
     }
 
-  def validate_config(self, config):
-    '''
-    [optional]
-
-    Harvesters can provide this method to validate the configuration
-    entered in the form. It should return a single string, which will be
-    stored in the database.  Exceptions raised will be shown in the form's
-    error messages.
-
-    :param harvest_object_id: Config string coming from the form
-    :returns: A string with the validated configuration options
-    '''
-    return ''
-
   def get_original_url(self, harvest_object_id):
     raise NotImplementedError("Not implemented")
 
   def _get_dataset_notes(self, root, dataset_name):
-    path = os.path.join(root, dataset_name)+".desc"
-
-    if(os.path.isfile(path)):
-      file = open(path, mode='r')
-      content = file.read()
-      file.close()
+    path = os.path.join(root, dataset_name)+".md"
+    try:
+      with open(path) as file:
+        content = file.read()
       return content
-    else:
+    except:
       return ""
+
+  def _get_dataset_infos(self, root, dataset_name):
+    path = os.path.join(root, dataset_name)+".json"
+    try:
+      with open(path) as json_file:
+        data = json.load(json_file)
+      return data
+    except:
 
   def gather_stage(self, harvest_job):
     '''
+    harvestJob.source content : {'_sa_instance_state': , 'frequency': 'ALWAYS', 'user_id': '', 'active': True, 'created': datetime.datetime(2021, 5, 15, 22, 43, 30, 458741), 'description': '', 'url': 'dataset_1', 'next_run': None, 'publisher_id': '', 'type': 'localfolders', 'config': '', 'title': 'dataset_1_title', 'id': 'e613a12e-e216-4f79-90ad-ec71b100f501'}
     :param harvest_job: HarvestJob object
     :returns: A list of HarvestObject ids
     '''
-    full_url = base_url+harvest_job.source.url
-
-    #{'_sa_instance_state': , 'frequency': 'ALWAYS', 'user_id': '', 'active': True, 'created': datetime.datetime(2021, 5, 15, 22, 43, 30, 458741), 'description': '', 'url': 'dataset_1', 'next_run': None, 'publisher_id': '', 'type': 'localfolders', 'config': '', 'title': 'dataset_1_title', 'id': 'e613a12e-e216-4f79-90ad-ec71b100f501'}
-
-    log.info("In gather stage: %s" % full_url)
     objs_ids = []
+    full_url = base_url+harvest_job.source.url
+    log.info("In gather stage: %s" % full_url)
 
     for (root, dirs, files) in os.walk(full_url):
       log.info("Harvest folder : "+str(root))
 
       for cur_dir in dirs:
+
+        notes = self._get_dataset_notes(root, cur_dir)
+        metadata = self._get_dataset_metadata(root, cur_dir)
 
         for (sub_root, sub_dirs, sub_files) in os.walk( os.path.join(full_url,cur_dir) ):
 
@@ -93,7 +86,8 @@ class LocalFoldersHarvester(HarvesterBase):
               "private" : False,
               "name" : name,
               "resources" : resources,
-              "notes" : self._get_dataset_notes(root, cur_dir)
+              "notes" : notes,
+              "tags" : metadata['tags']
             }
 
             obj = HarvestObject(guid=harvest_job.source.id+name,
